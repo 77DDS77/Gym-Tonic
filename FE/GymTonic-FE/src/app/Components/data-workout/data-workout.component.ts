79d0bfb5b02fa8e7474e24840b706e40d38684ee
77DDS77 <107913330+77DDS77@users.noreply.github.com>
2022-12-06 16:32:57 +0100

    iniziato workout update

--- FE/GymTonic-FE/src/app/Components/data-workout/data-workout.component.ts
+++ FE/GymTonic-FE/src/app/Components/data-workout/data-workout.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { faPencilAlt, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { UserExercise } from 'src/app/Models/user-exercise';
 import { Workout } from 'src/app/Models/workout';
 import { AuthService } from 'src/app/Services/auth.service';
 import { WorkoutService } from 'src/app/Services/workout.service';
@@ -12,6 +14,9 @@ import { WorkoutService } from 'src/app/Services/workout.service';
 export class DataWorkoutComponent implements OnInit {
 
   @Input() wrk!:Workout;
+  editWorkout!:FormGroup
+
+  workoutExercises: UserExercise[] = [];
 
   deleted:boolean = false;
 
@@ -24,7 +29,19 @@ export class DataWorkoutComponent implements OnInit {
     ) { }
 
   ngOnInit(): void {
+    this.workoutExercises = this.wrk.userExercises;
+    this.editWorkout = new FormGroup({
+      name: new FormControl('', Validators.required)
+    })
   }
+
+  removeExercise(ex:UserExercise){
+    const index = this.workoutExercises.indexOf(ex, 0);
+    this.workoutExercises.splice(index, 1);
+  }
+
+  updateWorkout(){}
+
 
   deleteWorkout(workoutId:number){
     let userId = this.auth.getLoggedUser().id;
