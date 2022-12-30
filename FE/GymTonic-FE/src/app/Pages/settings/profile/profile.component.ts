96aedc28da701fd2edf380ea5f6e9ba4b33b874a
77DDS77 <107913330+77DDS77@users.noreply.github.com>
2022-12-30 13:25:43 +0100

    user profile mngmnt

--- FE/GymTonic-FE/src/app/Pages/settings/profile/profile.component.ts
+++ FE/GymTonic-FE/src/app/Pages/settings/profile/profile.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { JwtUser } from 'src/app/Models/jwt-user';
+import { AuthService } from 'src/app/Services/auth.service';
+import { UserService } from 'src/app/Services/user.service';
 
 @Component({
   selector: 'app-profile',
@@ -7,9 +10,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProfileComponent implements OnInit {
 
-  constructor() { }
+  user!:JwtUser | undefined;
+
+  constructor(
+    private auth:AuthService,
+    private userSvc:UserService
+  ) { }
 
   ngOnInit(): void {
+    this.userSvc.getById(this.auth.getLoggedUser().id)
+    .subscribe({
+      next: (user) => this.user = user,
+      error: (err) => console.error(err)
+    })
   }
 
 }
